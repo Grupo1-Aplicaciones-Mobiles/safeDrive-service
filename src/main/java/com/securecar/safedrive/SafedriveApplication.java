fb6093c2aecfb7f427dafd1f6f023d55160d7767
Juan Diego <164110630+JDu202012207@users.noreply.github.com>
2024-09-13 20:53:06 -0500

    chore: initial commit.

---
+++ src/main/java/com/securecar/safedrive/SafedriveApplication.java
@@ -0,0 +1,13 @@
+package com.securecar.safedrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafedriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafedriveApplication.class, args);
+	}
+
+}
