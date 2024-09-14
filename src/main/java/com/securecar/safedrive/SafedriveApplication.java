ad962d74935b6427ba8b660a6756919885a17350
Juan Diego Cueto Dominguez <u202012207@upc.edu.pe>
2024-09-13 21:08:05 -0500

    feat(iam): jpa auditing implemented.

--- src/main/java/com/securecar/safedrive/SafedriveApplication.java
+++ src/main/java/com/securecar/safedrive/SafedriveApplication.java
@@ -2,8 +2,10 @@ package com.securecar.safedrive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SafedriveApplication {
 
 	public static void main(String[] args) {
