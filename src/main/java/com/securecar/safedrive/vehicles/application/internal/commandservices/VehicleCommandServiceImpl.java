67156f954db9d1c2433a75a16fbcd70bddf56ccc
paolomv02 <u202010039@upc.edu.pe>
2024-09-15 15:17:11 -0500

    feat(vehicles): implement update command

--- src/main/java/com/securecar/safedrive/vehicles/application/internal/commandservices/VehicleCommandServiceImpl.java
+++ src/main/java/com/securecar/safedrive/vehicles/application/internal/commandservices/VehicleCommandServiceImpl.java
@@ -2,6 +2,7 @@ package com.securecar.safedrive.vehicles.application.internal.commandservices;
 
 import com.securecar.safedrive.vehicles.domain.model.aggregates.Vehicle;
 import com.securecar.safedrive.vehicles.domain.model.commands.CreateVehicleCommand;
+import com.securecar.safedrive.vehicles.domain.model.commands.UpdateVehicleCommand;
 import com.securecar.safedrive.vehicles.domain.services.VehicleCommandService;
 import com.securecar.safedrive.vehicles.infrastructure.persistence.jpa.VehicleRepository;
 import org.springframework.stereotype.Service;
@@ -24,5 +25,39 @@ public class VehicleCommandServiceImpl implements VehicleCommandService {
         var vehicle = new Vehicle(command);
         var createdVehicle = vehicleRepository.save(vehicle);
         return Optional.of(createdVehicle);
+    }
+
+    @Override
+    public Optional<Vehicle> handle(UpdateVehicleCommand command) {
+        validateUpdateVehicleCommand(command);
+        var result = vehicleRepository.findById(command.id());
+        if (result.isEmpty()) {
+            throw new IllegalArgumentException("Vehicle does not exist");
+        }
+        var vehicleToUpdate = result.get();
+        try {
+            var updatedVehicle = vehicleRepository.save(vehicleToUpdate.updateInformation(command.marca(), command.modelo(), command.color(), command.placa()));
+            return Optional.of(updatedVehicle);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Error while updating vehicle: " + e.getMessage());
+        }
+    }
+
+    void validateUpdateVehicleCommand(UpdateVehicleCommand command) {
+        if (command.id() == null) {
+            throw new IllegalArgumentException("Vehicle id is required");
+        }
+        if (command.marca() == null) {
+            throw new IllegalArgumentException("Vehicle marca is required");
+        }
+        if (command.modelo() == null) {
+            throw new IllegalArgumentException("Vehicle modelo is required");
+        }
+        if (command.color() == null) {
+            throw new IllegalArgumentException("Vehicle color is required");
+        }
+        if (command.placa() == null) {
+            throw new IllegalArgumentException("Vehicle placa is required");
+        }
     }
 }
