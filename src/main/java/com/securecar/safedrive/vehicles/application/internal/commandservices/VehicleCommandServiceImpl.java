d53a36fd9c927b2fc558ed5fc0e0dd7f256a3d6d
MoisesD19 <moisesdonayre19@gmail.com>
2024-09-15 18:41:11 -0500

    feat(vehicles): added and implement delete vehicle

--- src/main/java/com/securecar/safedrive/vehicles/application/internal/commandservices/VehicleCommandServiceImpl.java
+++ src/main/java/com/securecar/safedrive/vehicles/application/internal/commandservices/VehicleCommandServiceImpl.java
@@ -2,6 +2,7 @@ package com.securecar.safedrive.vehicles.application.internal.commandservices;
 
 import com.securecar.safedrive.vehicles.domain.model.aggregates.Vehicle;
 import com.securecar.safedrive.vehicles.domain.model.commands.CreateVehicleCommand;
+import com.securecar.safedrive.vehicles.domain.model.commands.DeleteVehicleCommand;
 import com.securecar.safedrive.vehicles.domain.services.VehicleCommandService;
 import com.securecar.safedrive.vehicles.infrastructure.persistence.jpa.VehicleRepository;
 import org.springframework.stereotype.Service;
@@ -24,5 +25,15 @@ public class VehicleCommandServiceImpl implements VehicleCommandService {
         var vehicle = new Vehicle(command);
         var createdVehicle = vehicleRepository.save(vehicle);
         return Optional.of(createdVehicle);
+    }
+
+    @Override
+    public void handle(DeleteVehicleCommand command) {
+        Optional<Vehicle> vehicle = vehicleRepository.findById(command.id());
+        if (vehicle.isPresent()) {
+            vehicleRepository.delete(vehicle.get());
+        } else {
+            throw new IllegalArgumentException("Vehicle with ID " + command.id() + " not found");
+        }
     }
 }
--- src/main/java/com/securecar/safedrive/vehicles/domain/services/VehicleCommandService.java
+++ src/main/java/com/securecar/safedrive/vehicles/domain/services/VehicleCommandService.java
@@ -2,9 +2,11 @@ package com.securecar.safedrive.vehicles.domain.services;
 
 import com.securecar.safedrive.vehicles.domain.model.aggregates.Vehicle;
 import com.securecar.safedrive.vehicles.domain.model.commands.CreateVehicleCommand;
+import com.securecar.safedrive.vehicles.domain.model.commands.DeleteVehicleCommand;
 
 import java.util.Optional;
 
 public interface VehicleCommandService {
     Optional<Vehicle> handle(CreateVehicleCommand createVehicleCommand);
+    void handle(DeleteVehicleCommand command);
 }
