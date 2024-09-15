17f8c5dff8e55cbe6c1a9c46ef4d6d8c92127045
paolomv02 <u202010039@upc.edu.pe>
2024-09-15 15:16:57 -0500

    feat(vehicles): add update command

---
+++ src/main/java/com/securecar/safedrive/vehicles/domain/model/commands/UpdateVehicleCommand.java
@@ -0,0 +1,4 @@
+package com.securecar.safedrive.vehicles.domain.model.commands;
+
+public record UpdateVehicleCommand(Long id, String marca, String modelo, String color, String placa) {
+}
--- src/main/java/com/securecar/safedrive/vehicles/domain/services/VehicleCommandService.java
+++ src/main/java/com/securecar/safedrive/vehicles/domain/services/VehicleCommandService.java
@@ -2,9 +2,11 @@ package com.securecar.safedrive.vehicles.domain.services;
 
 import com.securecar.safedrive.vehicles.domain.model.aggregates.Vehicle;
 import com.securecar.safedrive.vehicles.domain.model.commands.CreateVehicleCommand;
+import com.securecar.safedrive.vehicles.domain.model.commands.UpdateVehicleCommand;
 
 import java.util.Optional;
 
 public interface VehicleCommandService {
     Optional<Vehicle> handle(CreateVehicleCommand createVehicleCommand);
+    Optional<Vehicle> handle(UpdateVehicleCommand updateVehicleCommand);
 }
