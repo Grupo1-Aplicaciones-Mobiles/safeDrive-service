fe0c433f2cdfbef488e6f0a9936d7046f4f146ea
JDu202012207 <u202012207@upc.edu.pe>
2024-11-10 15:34:27 -0500

    refactor: update deployment settings.

--- src/main/java/com/securecar/safedrive/iam/infrastructure/authorization/sfs/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/securecar/safedrive/iam/infrastructure/authorization/sfs/configuration/WebSecurityConfiguration.java
@@ -105,7 +105,6 @@ public class WebSecurityConfiguration {
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
                         .requestMatchers(
                                 "/api/v1/authentication/**",
-                                "/api/v1/**",
                                 "/v3/api-docs/**",
                                 "/swagger-ui.html",
                                 "/swagger-ui/**",
